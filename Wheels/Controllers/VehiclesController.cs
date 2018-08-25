089ed5037cb33e4e21cdfac5ae0e97e0d3046163
Valentyn Demchuk <vdemchuk.vd@gmail.com>
2018-08-25 23:44:59 +0300

    adding a record to the database

---
+++ Wheels/Controllers/VehiclesController.cs
@@ -0,0 +1,40 @@
+﻿using AutoMapper;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wheels.Controllers.Resources;
+using Wheels.Models;
+using Wheels.Persistence;
+
+namespace Wheels.Controllers
+{
+	[Route("/api/vehicles")]
+
+    public class VehicleController : Controller
+    {
+		private readonly IMapper mapper;
+
+		private readonly WheelsDbContext context;
+
+		public VehicleController(IMapper mapper, WheelsDbContext context)
+		{
+			this.mapper = mapper;
+			this.context = context;
+		}
+
+		[HttpPost]
+		public async Task<IActionResult> CreateVehicle(VehicleResource vehicleResource)
+		{
+			var vehicle = mapper.Map<VehicleResource, Vehicle>(vehicleResource);
+			vehicle.LastUpdate = DateTime.Now;
+
+			context.Vehicles.Add(vehicle);
+			await context.SaveChangesAsync();
+
+			var result = mapper.Map<Vehicle, VehicleResource>(vehicle);
+			return Ok(result);
+		}
+    }
+}
