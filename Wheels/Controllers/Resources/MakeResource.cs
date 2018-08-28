9fc24f12577af75abadaa91aad18f8bba9359a10
Valentyn Demchuk <vdemchuk.vd@gmail.com>
2018-08-28 23:14:10 +0300

    refactoring using KeyValuePairResource

--- Wheels/Controllers/Resources/MakeResource.cs
+++ Wheels/Controllers/Resources/MakeResource.cs
@@ -7,17 +7,13 @@ using static Microsoft.EntityFrameworkCore.DbLoggerCategory;
 
 namespace Wheels.Controllers.Resources
 {
-    public class MakeResource
+    public class MakeResource : KeyValuePairResource
     {
-		public int Id { get; set; }
-
-		public string Name { get; set; }
-
-		public ICollection<ModelResource> Models { get; set; }
+		public ICollection<KeyValuePairResource> Models { get; set; }
 
 		public MakeResource()
 		{
-			Models = new Collection<ModelResource>();
+			Models = new Collection<KeyValuePairResource>();
 		}
 	}
 }
--- Wheels/Controllers/Resources/VehicleResource.cs
+++ Wheels/Controllers/Resources/VehicleResource.cs
@@ -10,9 +10,9 @@ namespace Wheels.Controllers.Resources
     {
 		public int Id { get; set; }
 
-		public ModelResource Model { get; set; }
+		public KeyValuePairResource Model { get; set; }
 
-		public MakeResource Make { get; set; }
+		public KeyValuePairResource Make { get; set; }
 
 		public bool IsRegistered { get; set; }
 
@@ -20,11 +20,11 @@ namespace Wheels.Controllers.Resources
 
 		public DateTime LastUpdate { get; set; }
 
-		public ICollection<FeatureResource> Features { get; set; }
+		public ICollection<KeyValuePairResource> Features { get; set; }
 
 		public VehicleResource()
 		{
-			Features = new Collection<FeatureResource>();
+			Features = new Collection<KeyValuePairResource>();
 		}
 	}
 }
