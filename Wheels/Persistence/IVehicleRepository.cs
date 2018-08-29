1120bbc81075687835a78ef574b8e26b7458c671
Valentyn Demchuk <vdemchuk.vd@gmail.com>
2018-08-29 23:34:17 +0300

    dependency injection

---
+++ Wheels/Persistence/IVehicleRepository.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Wheels.Models;
+
+namespace Wheels.Persistence
+{
+    public interface IVehicleRepository
+    {
+		Task<Vehicle> GetVehicle(int id);
+
+	}
+}
