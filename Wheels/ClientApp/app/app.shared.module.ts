0583d99eaf8b5f548af7669a41ebc47f2c4bea33
Valentyn Demchuk <vdemchuk.vd@gmail.com>
2018-09-10 09:40:26 +0300

    without toasty (stable)

---
+++ Wheels/ClientApp/app/app.shared.module.ts
@@ -0,0 +1,43 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+import { RouterModule } from '@angular/router';
+
+import { AppComponent } from './components/app/app.component';
+import { NavMenuComponent } from './components/navmenu/navmenu.component';
+import { HomeComponent } from './components/home/home.component';
+import { FetchDataComponent } from './components/fetchdata/fetchdata.component';
+import { CounterComponent } from './components/counter/counter.component';
+import { VehicleFormComponent } from './components/vehicle-module/vehicle-form/vehicle-form.component';
+import { VehicleService } from './services/vehicle.service';
+
+@NgModule({
+	bootstrap: [AppComponent],
+    declarations: [
+        AppComponent,
+        NavMenuComponent,
+        CounterComponent,
+        FetchDataComponent,
+		HomeComponent,
+		VehicleFormComponent 
+    ],
+    imports: [
+        CommonModule,
+        HttpModule,
+        FormsModule,
+        RouterModule.forRoot([
+			{ path: '', redirectTo: 'home', pathMatch: 'full' },
+			{ path: 'vehicles/new', component: VehicleFormComponent },
+            { path: 'home', component: HomeComponent },
+            { path: 'counter', component: CounterComponent },
+            { path: 'fetch-data', component: FetchDataComponent },
+            { path: '**', redirectTo: 'home' }
+        ])
+	],
+	providers: [
+		VehicleService
+	]
+})
+export class AppModuleShared {
+}
